9f5cf1d431993e21c41df0fdadfcc480e73aaa00
lmrigo <thiaguinhojr@gmail.com>
2014-01-30 15:17:49 -0200

    trying to count subsequent AB

--- test.py
+++ test.py
@@ -1,8 +1,9 @@
-input = "123456789"
+input1 = "123456789"
 input2 = "123454321"
 input3 = "12345678910"
 input4 = "1234567891011"
 input5 = "1101211"
+input6 = "11111"
 
 lowercaseOffset = 96
 def decode (strNum):
@@ -24,33 +25,30 @@ def translate (input):
 #comboB = {'ba':'u','bb':'v','bc':'w','bd':'x','be':'y','bf':'z'}
 comboA = ['aa','ab','ac','ad','ae','af','ag','ah','ai']
 comboB = ['ba','bb','bc','bd','be','bf']
-def counter (translated,m):
+def counter (translated,previousAB):
 	if len(translated) <= 1:
-		return 0
+		return 1.0
 	duet = translated[0]+translated[1]
 	if duet in comboA or duet in comboB:
-		return 2 + counter(translated[1:],0)
+		if previousAB:
+			return (3.0/2.0) * counter(translated[1:],False)
+		else:
+			return 2.0 * counter(translated[1:],True)
 	else:
-		return counter(translated[1:],0)
+		return counter(translated[1:],False)
 
-print input5 # 1101211
-print translate(input5) # ajabaa
-print counter(translate(input5),0) # 6
+print 'input1:' + str(counter(translate(input1),False) == 3.0) + ' 3<=>' + str(counter(translate(input1),False))
+print 'input2:' + str(counter(translate(input2),False) == 6.0) + ' 6<=>' + str(counter(translate(input2),False))
+print 'input3:' + str(counter(translate(input3),False) == 3.0) + ' 3<=>' + str(counter(translate(input3),False))
+print 'input4:' + str(counter(translate(input4),False) == 6.0) + ' 6<=>' + str(counter(translate(input4),False))
+print 'input5:' + str(counter(translate(input5),False) == 5.0) + ' 5<=>' + str(counter(translate(input5),False))
+print 'input6:' + str(counter(translate(input5),False) == 5.0) + ' ?<=>' + str(counter(translate(input5),False))
+
 """
-1. ajabaa
-2. ajabk
-3. ajaua
-4. ajlaa
-5. ajlk
-"""
-"""
-print input4 # 1234567891011
-print translate(input4) # abcdefghijaa
-print counter(translate(input4),0) # 6
-1. abcdefghijaa
-2. abcdefghijk
-3. lcdefghijaa
-4. lcdefghijk
-5. awdefghijaa
-6. awdefghijk
+input1 = "123456789"
+input2 = "123454321"
+input3 = "12345678910"
+input4 = "1234567891011"
+input5 = "1101211"
+input6 = "11111"
 """
